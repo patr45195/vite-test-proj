62189d85d8dbe8466dde5c56e322562f1e259909
patr45195 <patr45195@gmail.com>
2024-10-22 20:41:08 +0300

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import "./App.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <div>Init commit</div>
-    </>
+    <div className="app-container">
+      <Header />
+      <div className="content-container">
+        <Navbar />
+        <div className="main-content">
+          <h1>Main content area</h1>
+          <p>This my page</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
